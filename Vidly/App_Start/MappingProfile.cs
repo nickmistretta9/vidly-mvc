e6b1658eeea9b7fcc180a45d284dc6179767771a
Nick Mistretta <nickmistretta9@gmail.com>
2018-09-20 22:36:25 -0400

    Added jquery api consumption and client-side rendering of data tables. Added datatables jquery plugin for better performing data tables and bootbox for bootstrap-like modal confirmation boxes

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,9 +12,13 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
+            // Domain to DTO
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+            // DTO to Domain
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
